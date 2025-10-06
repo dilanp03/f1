37e2700d58d17fa410372160c637520c41d31362
Dilan <dp1g21@soton.ac.uk>
2025-10-06 14:40:12 +0100

    Modified notebook for COTA

--- f1_1.py
+++ f1_1.py
@@ -5,14 +5,14 @@ os.makedirs('cache_dir', exist_ok=True)
 fastf1.Cache.enable_cache('cache_dir')
 
 schedule = fastf1.get_event_schedule(2025)
-#print(schedule)
-#print(schedule.columns)
-#print(schedule.loc[:,['RoundNumber', 'Country', 'Location', 'EventFormat', 'Session5', 'Session5DateUtc']])
+print(schedule)
+print(schedule.columns)
+print(schedule.loc[:,['RoundNumber', 'Country', 'Location', 'EventFormat', 'Session5', 'Session5DateUtc']])
 
-session = fastf1.get_session(2025,7,'R')
-session.load()
-results = session.results.loc[:,['Abbreviation', 'TeamName', 'ClassifiedPosition', 'Points']]
-print(results)
+# session = fastf1.get_session(2025,7,'R')
+# session.load()
+# results = session.results.loc[:,['Abbreviation', 'TeamName', 'ClassifiedPosition', 'Points']]
+# print(results)
 
-fastest = session.laps.pick_fastest()
-print(fastest)
+# fastest = session.laps.pick_fastest()
+# print(fastest)
